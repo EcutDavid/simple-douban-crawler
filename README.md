007ebbbff2ae2badf3135e86a35c58a14800bdce
David Guan <DavidGuanDev@Gmail.com>
2016-11-27 14:48:05 +0800

    Decode page content using utf-8

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ## Environment
 python 3.5
+
+## References
+[BeautifulSoup](http://beautiful-soup-4.readthedocs.io/en/latest/)
--- index.py
+++ index.py
@@ -2,11 +2,15 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
+DOUBAN_SHANGHAI_URL = 'https://shanghai.douban.com/'
+
 try:
-    html = urlopen('https://www.google.com/').read()
+    with urlopen(DOUBAN_SHANGHAI_URL) as response:
+        html = response.read().decode('utf-8')
 except Exception as e:
     print(e)
 
 if html != None:
     pageObj = BeautifulSoup(html, 'html.parser')
-    print(len(pageObj.findAll('div')))
+    for div in pageObj.findAll('div'):
+        print(div.get_text())
